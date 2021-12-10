f614018d0bbec78525d96d6d629b26d2343cb0e8
Murillo Alves <tumusx1@gmail.com>
2021-12-09 22:33:49 -0300

    implement do while and while

--- src/OOP/condtional/Conditionalif.kt
+++ src/OOP/condtional/Conditionalif.kt
@@ -13,6 +13,30 @@ fun mainSomaNotas (n1 : Int, n2 : Int) : Int {
         return n2 //se n1 for maior que n2, retorna o valor de n2
     }
 }
+
+fun forLoop(){
+    val items = listOf("maçaKaiana", "bananaOrange", "kiwiSirlon")
+    for (item in items) {
+        println(item)
+    }
+
+}
+
+//while é utilizado quando vc quer verificar somente a condição verdadeira (nao sabemos quando vai terminar)
+// enquanto o do while vai executar pelo menos uma vez até q seja verificado se é verdade ou falso
+//percorrer
+
+fun whileLoop(){
+    var itens = listOf("macaPus", "macaCaiana", "MacaKiw")
+    var index = 0
+    while(index<itens.size){
+        println("item at $index is ${itens[index]}")
+        index++
+    }
+}
+
 fun main(){
+    forLoop()
+    whileLoop()
     println(mainSomaNotas(25,20))
 }
